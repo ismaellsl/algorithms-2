89f7b02bf4632abc6cc028320cb582d2881211b2
Ismaell Oliveira <ismaell.oliveira@sap.com>
2018-11-11 23:13:36 -0200

    project structure

---
+++ src/main/java/com/unisinos/algorithms/Domino.java
@@ -0,0 +1,23 @@
+package com.unisinos.algorithms;
+
+public class Domino {
+	private int up;
+	private int down;
+
+	public int getUp() {
+		return up;
+	}
+
+	public int getDown() {
+		return down;
+	}
+
+	public void setUp(int up) {
+		this.up = up;
+	}
+
+	public void setDown(int down) {
+		this.down = down;
+	}
+
+}
---
+++ src/main/java/com/unisinos/algorithms/DominoOperations.java
@@ -0,0 +1,14 @@
+package com.unisinos.algorithms;
+
+import java.util.List;
+
+public class DominoOperations {
+
+	private  List<Domino> dominoResult;
+	private int sumUp;
+	private int sumDown;
+
+	public int (List<Domino> dominos) {
+
+	}
+}
